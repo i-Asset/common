c09d5455d63cdd6c89e75dedb33fd692ebcbd9b0
Dietmar Glachs <dglachs@gmail.com>
2020-12-18 11:39:54 +0100

    restore functionality

--- solr-projects/solr-model/src/main/java/at/srfg/iot/common/solr/model/model/party/IParty.java
+++ solr-projects/solr-model/src/main/java/at/srfg/iot/common/solr/model/model/party/IParty.java
@@ -22,4 +22,23 @@ public interface IParty extends IConcept {
 	public String BUSINESS_TYPE_FIELD = "businessType";
 	public String ACTIVITY_SECTORS_FIELD = "*_activitySectors";
 	public String BUSINESS_KEYWORDS_FIELD = "*_businessKeywords";
+	
+	public String BRAND_NAME_FIELD = "*_brandName";
+	public String LOWERCASE_LEGAL_NAME_FIELD = "lowercaseLegalName";
+	public String NAME_FIELD = "name";
+	public String NAME_ML_FIELD = "*_name";
+	public String ORIGIN_FIELD = "*_origin";
+	public String CERTIFICATE_TYPE_FIELD = "*_certificateType";
+	public String PPAP_COMPLIANCE_LEVEL_FIELD = "ppapComplianceLevel";
+	public String PPAP_DOCUMENT_TYPE_FIELD = "*_ppapDocumentType";
+	public String TRUST_SCORE_FIELD = "trustScore";
+	public String TRUST_RATING_FIELD = "trustRating";
+	public String TRUST_TRADING_VOLUME_FIELD = "trustTradingVolume";
+	public String TRUST_SELLLER_COMMUNICATION_FIELD = "trustSellerCommunication";
+	public String TRUST_FULFILLMENT_OF_TERMS_FIELD = "trustFullfillmentOfTerms";
+	public String TRUST_DELIVERY_PACKAGING_FIELD = "trustDeliveryPackaging";
+	public String TRUST_NUMBER_OF_TRANSACTIONS_FIELD = "trustNumberOfTransactions";
+	public String TRUST_NUMBER_OF_EVALUATIONS_FIELD = "trustNumberOfEvaluations";
+	public String VERIFIED_FIELD = "verified";
+	public String WEBSITE_FIELD = "website";	
 }
--- solr-projects/solr-model/src/main/java/at/srfg/iot/common/solr/model/model/party/PartyType.java
+++ solr-projects/solr-model/src/main/java/at/srfg/iot/common/solr/model/model/party/PartyType.java
@@ -2,6 +2,7 @@ package at.srfg.iot.common.solr.model.model.party;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 import org.springframework.data.annotation.ReadOnlyProperty;
@@ -31,7 +32,44 @@ public class PartyType extends Concept implements IParty, ICustomPropertyAware {
 	@Indexed(name=LEGAL_ENTITY_IDENTIFIER_FIELD)
 	private String legalEntityIdentifier;
 
+	@Indexed(name=BRAND_NAME_FIELD) @Dynamic
+	private Map<String, String> brandName;
+	@Indexed(name=ORIGIN_FIELD) @Dynamic
+	private Map<String,String> origin;
+	@Indexed(name=CERTIFICATE_TYPE_FIELD, type="string") @Dynamic
+	private Map<String,Collection<String>> certificateType;
+	@Indexed(name=PPAP_COMPLIANCE_LEVEL_FIELD, type="pint")
+	private Integer ppapComplianceLevel;
+	@Indexed(name=PPAP_DOCUMENT_TYPE_FIELD) @Dynamic
+	private Map<String,String> ppapDocumentType;
+	@Indexed(name=TRUST_SCORE_FIELD, type="pdouble")
+	private Double trustScore;
+	@Indexed(name=TRUST_RATING_FIELD, type="pdouble")
+	private Double trustRating;
+	@Indexed(name=TRUST_TRADING_VOLUME_FIELD, type="pdouble")
+	private Double trustTradingVolume;
+	@Indexed(name=TRUST_SELLLER_COMMUNICATION_FIELD, type="pdouble")
+	private Double trustSellerCommunication;
+	@Indexed(name=TRUST_FULFILLMENT_OF_TERMS_FIELD, type="pdouble")
+	private Double trustFullfillmentOfTerms;
+	@Indexed(name=TRUST_DELIVERY_PACKAGING_FIELD, type="pdouble")
+	private Double trustDeliveryPackaging;
+	@Indexed(name=TRUST_NUMBER_OF_TRANSACTIONS_FIELD, type="pdouble")
+	private Double trustNumberOfTransactions;
+	@Indexed(name=TRUST_NUMBER_OF_EVALUATIONS_FIELD, type="pdouble",required=false)
+	private Double trustNumberOfEvaluations;
+	@Indexed(name= ACTIVITY_SECTORS_FIELD, type="string")
+	@Dynamic
+	private Map<String, Collection<String>> activitySectors;
+	@Indexed(name=BUSINESS_KEYWORDS_FIELD, type="string")
+	@Dynamic
+	protected Map<String, Collection<String>> businessKeywords;
 
+	@Indexed(name=VERIFIED_FIELD,type="boolean")
+	private Boolean isVerified = false;
+
+	@Indexed(name=WEBSITE_FIELD, type="string")
+	private String website;
 //	@Indexed(name= ACTIVITY_SECTORS_FIELD, type=SOLR_STRING)
 //	@Dynamic
 //	private Map<String, Collection<String>> activitySectors;
@@ -164,5 +202,270 @@ public class PartyType extends Concept implements IParty, ICustomPropertyAware {
 	 */
 	public void setLegalEntityIdentifier(String legalEntityIdentifier) {
 		this.legalEntityIdentifier = legalEntityIdentifier;
+	}
+	/**
+	 * @return the brandName
+	 */
+	public Map<String, String> getBrandName() {
+		return brandName;
+	}
+	/**
+	 * @param brandName the brandName to set
+	 */
+	public void setBrandName(Map<String, String> brandName) {
+		this.brandName = brandName;
+	}
+	/**
+	 * @return the origin
+	 */
+	public Map<String, String> getOrigin() {
+		return origin;
+	}
+	/**
+	 * @param origin the origin to set
+	 */
+	public void setOrigin(Map<String, String> origin) {
+		this.origin = origin;
+	}
+	
+	public Map<String,Collection<String>> getCertificateType() {
+		return certificateType;
+	}
+
+	/**
+	 * Setter for the certificateType labels
+	 * @param certificateTypeMap
+	 */
+	public void setCertificateType(Map<String, Collection<String>> certificateTypeMap) {
+		if ( certificateTypeMap != null ) {
+			for ( String lang : certificateTypeMap.keySet() ) {
+				for (String label : certificateTypeMap.get(lang)) {
+					addCertificateType(lang, label);
+					
+				}
+			}
+		}
+		else {
+			this.certificateType = null;
+		}
+	}
+	/**
+	 * Helper method for adding a (multilingual) label to the list of certificat types 
+	 * @param language
+	 * @param certificatTypeLabel
+	 */
+	public void addCertificateType(String language, String certificatTypeLabel) {
+		if (this.certificateType ==null) {
+			this.certificateType = new HashMap<>();
+		}
+		if ( !this.certificateType.containsKey(language)) {
+			this.certificateType.put(language, new HashSet<>());
+		}
+		this.certificateType.get(language).add(certificatTypeLabel);
+		// 
+		addLanguage(language);
+	}	/**
+	 * @return the ppapComplianceLevel
+	 */
+	public Integer getPpapComplianceLevel() {
+		return ppapComplianceLevel;
+	}
+	/**
+	 * @param ppapComplianceLevel the ppapComplianceLevel to set
+	 */
+	public void setPpapComplianceLevel(Integer ppapComplianceLevel) {
+		this.ppapComplianceLevel = ppapComplianceLevel;
+	}
+	/**
+	 * @return the ppapDocumentType
+	 */
+	public Map<String, String> getPpapDocumentType() {
+		return ppapDocumentType;
+	}
+	/**
+	 * @param ppapDocumentType the ppapDocumentType to set
+	 */
+	public void setPpapDocumentType(Map<String, String> ppapDocumentType) {
+		this.ppapDocumentType = ppapDocumentType;
+	}
+	/**
+	 * @return the trustScore
+	 */
+	public Double getTrustScore() {
+		return trustScore;
+	}
+	/**
+	 * @param trustScore the trustScore to set
+	 */
+	public void setTrustScore(Double trustScore) {
+		this.trustScore = trustScore;
+	}
+	/**
+	 * @return the trustRating
+	 */
+	public Double getTrustRating() {
+		return trustRating;
+	}
+	/**
+	 * @param trustRating the trustRating to set
+	 */
+	public void setTrustRating(Double trustRating) {
+		this.trustRating = trustRating;
+	}
+	/**
+	 * @return the trustTradingVolume
+	 */
+	public Double getTrustTradingVolume() {
+		return trustTradingVolume;
+	}
+	/**
+	 * @param trustTradingVolume the trustTradingVolume to set
+	 */
+	public void setTrustTradingVolume(Double trustTradingVolume) {
+		this.trustTradingVolume = trustTradingVolume;
+	}
+	/**
+	 * @return the trustSellerCommunication
+	 */
+	public Double getTrustSellerCommunication() {
+		return trustSellerCommunication;
+	}
+	/**
+	 * @param trustSellerCommunication the trustSellerCommunication to set
+	 */
+	public void setTrustSellerCommunication(Double trustSellerCommunication) {
+		this.trustSellerCommunication = trustSellerCommunication;
+	}
+	/**
+	 * @return the trustFullfillmentOfTerms
+	 */
+	public Double getTrustFullfillmentOfTerms() {
+		return trustFullfillmentOfTerms;
+	}
+	/**
+	 * @param trustFullfillmentOfTerms the trustFullfillmentOfTerms to set
+	 */
+	public void setTrustFullfillmentOfTerms(Double trustFullfillmentOfTerms) {
+		this.trustFullfillmentOfTerms = trustFullfillmentOfTerms;
+	}
+	/**
+	 * @return the trustDeliveryPackaging
+	 */
+	public Double getTrustDeliveryPackaging() {
+		return trustDeliveryPackaging;
+	}
+	/**
+	 * @param trustDeliveryPackaging the trustDeliveryPackaging to set
+	 */
+	public void setTrustDeliveryPackaging(Double trustDeliveryPackaging) {
+		this.trustDeliveryPackaging = trustDeliveryPackaging;
+	}
+	/**
+	 * @return the trustNumberOfTransactions
+	 */
+	public Double getTrustNumberOfTransactions() {
+		return trustNumberOfTransactions;
+	}
+	/**
+	 * @param trustNumberOfTransactions the trustNumberOfTransactions to set
+	 */
+	public void setTrustNumberOfTransactions(Double trustNumberOfTransactions) {
+		this.trustNumberOfTransactions = trustNumberOfTransactions;
+	}
+	/**
+	 * @return the trustNumberOfEvaluations
+	 */
+	public Double getTrustNumberOfEvaluations() {
+		return trustNumberOfEvaluations;
+	}
+	/**
+	 * @param trustNumberOfEvaluations the trustNumberOfEvaluations to set
+	 */
+	public void setTrustNumberOfEvaluations(Double trustNumberOfEvaluations) {
+		this.trustNumberOfEvaluations = trustNumberOfEvaluations;
+	}
+	/**
+	 * @return the activitySectors
+	 */
+	public Map<String, Collection<String>> getActivitySectors() {
+		return activitySectors;
+	}
+	/**
+	 * @param activitySectors the activitySectors to set
+	 */
+	public void setActivitySectors(Map<String, Collection<String>> activitySectors) {
+		this.activitySectors = activitySectors;
+	}
+	/**
+	 * @return the businessKeywords
+	 */
+	public Map<String, Collection<String>> getBusinessKeywords() {
+		return businessKeywords;
+	}
+	/**
+	 * @param businessKeywords the businessKeywords to set
+	 */
+	public void setBusinessKeywords(Map<String, Collection<String>> businessKeywords) {
+		this.businessKeywords = businessKeywords;
+	}
+	/**
+	 * @return the isVerified
+	 */
+	public Boolean getIsVerified() {
+		return isVerified;
+	}
+	/**
+	 * @param isVerified the isVerified to set
+	 */
+	public void setIsVerified(Boolean isVerified) {
+		this.isVerified = isVerified;
+	}
+	/**
+	 * @return the website
+	 */
+	public String getWebsite() {
+		return website;
+	}
+	/**
+	 * @param website the website to set
+	 */
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	/**
+	 * @param customIntValues the customIntValues to set
+	 */
+	public void setCustomIntValues(Map<String, Collection<Integer>> customIntValues) {
+		this.customIntValues = customIntValues;
+	}
+	/**
+	 * @param customDoubleValues the customDoubleValues to set
+	 */
+	public void setCustomDoubleValues(Map<String, Collection<Double>> customDoubleValues) {
+		this.customDoubleValues = customDoubleValues;
+	}
+	/**
+	 * @param customStringValues the customStringValues to set
+	 */
+	public void setCustomStringValues(Map<String, Collection<String>> customStringValues) {
+		this.customStringValues = customStringValues;
+	}
+	/**
+	 * @param customBooleanValue the customBooleanValue to set
+	 */
+	public void setCustomBooleanValue(Map<String, Boolean> customBooleanValue) {
+		this.customBooleanValue = customBooleanValue;
+	}
+	/**
+	 * @param customPropertyKeys the customPropertyKeys to set
+	 */
+	public void setCustomPropertyKeys(Map<String, String> customPropertyKeys) {
+		this.customPropertyKeys = customPropertyKeys;
+	}
+	/**
+	 * @param customProperties the customProperties to set
+	 */
+	public void setCustomProperties(Map<String, PropertyType> customProperties) {
+		this.customProperties = customProperties;
 	}
 }
