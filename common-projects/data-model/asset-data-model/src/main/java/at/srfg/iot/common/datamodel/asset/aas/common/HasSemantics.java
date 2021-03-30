aa33901913b5baab96857e9d0b778166bc1b448c
Dietmar Glachs <dglachs@gmail.com>
2021-03-30 11:21:43 +0200

    removal of isSemanticReferenceResolved - should be placed elsewhere

--- common-projects/data-model/asset-data-model/src/main/java/at/srfg/iot/common/datamodel/asset/aas/common/HasSemantics.java
+++ common-projects/data-model/asset-data-model/src/main/java/at/srfg/iot/common/datamodel/asset/aas/common/HasSemantics.java
@@ -48,7 +48,6 @@ public interface HasSemantics {
 			Referable semantic = getSemanticElement();
 			return semantic.asReference();
 		}
-		//return new Reference(getSemanticElement());
 	}
 	/**
 	 * Setter for the reference
@@ -57,27 +56,4 @@ public interface HasSemantics {
 	default void setSemanticId(Reference reference) {
 		setSemanticElement(reference);
 	}
-	/**
-	 * Convenience method to check whether a provided {@link Reference}
-	 * must be resolved and stored in the current {@link HasSemantics} with
-	 * {@link #setSemanticElement(ReferableElement)}
-	 * 
-	 * @param reference The reference pointing to a {@link ReferableElement}
-	 * 
-	 * @return <code>true</code> when the current {@link #getSemanticElement()} is referenced 
-	 * with the provided {@link Reference}, <code>false</code> to indicate that
-	 * the reference needs to be resolved!
-	 * 
-	 */
-	default boolean isSemanticReferenceResolved(Reference reference) {
-		if ( reference != null ) {
-			if ( getSemanticElement() != null  ) {
-				return getSemanticElement().asReference().equals(reference);		
-			}
-			return false;
-		}
-		return true;
-		// the current semantic element is null, the provided reference must not be null  
-	}
-
 }
