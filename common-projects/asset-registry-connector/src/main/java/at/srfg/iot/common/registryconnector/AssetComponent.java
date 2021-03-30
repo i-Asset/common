5281858b841baf6d1b73c1ced9d9b551f83f118c
Dietmar Glachs <dglachs@gmail.com>
2021-03-30 11:19:43 +0200

    Refactoring from IAssetModel to IAssetProvider

--- common-projects/asset-registry-connector/src/main/java/at/srfg/iot/common/registryconnector/AssetComponent.java
+++ common-projects/asset-registry-connector/src/main/java/at/srfg/iot/common/registryconnector/AssetComponent.java
@@ -1,13 +1,12 @@
 package at.srfg.iot.common.registryconnector;
 
-import at.srfg.iot.common.datamodel.asset.provider.IAssetModelListener;
-import at.srfg.iot.common.datamodel.asset.provider.IAssetProvider;
+import at.srfg.iot.common.aas.IAssetModel;
 
 public interface AssetComponent {
 //	default AssetComponent create(int port) {
 //		return new I40Component(port, "/");
 //	}
-	public void serve(IAssetProvider provider, String alias);
+	public void serve(IAssetModel provider, String alias);
 	/**
 	 * Start a I40 Component/Device, eg. start the REST service providing
 	 * external access to this device
@@ -18,5 +17,5 @@ public interface AssetComponent {
 	 */
 	public void stop();
 	
-	public void addModelListener(IAssetModelListener listener);
+	public boolean isStarted();
 }
